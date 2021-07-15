af74acc06b4f4657313e8ae0b197d570765a134c
Vladimir Ukalitsky <vovaz04h@gmail.com>
2021-07-15 21:39:05 +0300

    get posts from url

--- main.go
+++ main.go
@@ -1,4 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
 func main() {
+	url := "https://jsonplaceholder.typicode.com/posts"
+
+	resp, err := http.Get(url)
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	fmt.Println(string(body))
 }
